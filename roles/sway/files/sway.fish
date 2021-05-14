7c71d420bddc20ee4c9ae3ca211842ac9d310666
eoli3n <jonathan.kirszling@runbox.com>
2021-05-14 18:33:02 +0200

    syncthing and run dbus session manually

--- roles/sway/files/sway.fish
+++ roles/sway/files/sway.fish
@@ -10,6 +10,6 @@ set TTY1 (tty)
 
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
   # Debug
-  #exec sway -d 2> ~/sway.log
-  exec sway
+  #exec dbus-run-session sway -d 2> ~/sway.log
+  exec dbus-run-session sway
 end
