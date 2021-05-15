8051cb1acaf6695c6578d9d3a5c45e261c2a629a
eoli3n <jonathan.kirszling@runbox.com>
2021-05-15 20:59:01 +0200

    fixed XDG_DATA_DIRS

--- roles/fish/files/fish/desktop/conf.d/flatpak.fish
+++ roles/fish/files/fish/desktop/conf.d/flatpak.fish
@@ -1,3 +1,3 @@
 # Set path by host
 
-set -x XDG_DATA_DIRS "/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"
+set -x XDG_DATA_DIRS "$XDG_DATA_DIRS:/usr/share/:/usr/local/share/:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"
