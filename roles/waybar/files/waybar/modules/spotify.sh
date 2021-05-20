2bee64625386163b90395b2b69c535c853e44070
eoli3n <jonathan.kirszling@runbox.com>
2021-05-20 19:33:07 +0200

    fixed spotify sh to bash module script

--- roles/waybar/files/waybar/modules/spotify.sh
+++ roles/waybar/files/waybar/modules/spotify.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
-if [[ ${#info} > 40 ]]; then
+if [[ "${#info}" > 40 ]]; then
     info=$(echo $info | cut -c1-40)"..."
 fi
 
