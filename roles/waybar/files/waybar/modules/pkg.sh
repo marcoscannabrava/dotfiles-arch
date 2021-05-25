c3a1f1b401359310571af589719d973f3595a0da
eoli3n <jkirsz@gmail.com>
2021-05-25 15:33:50 +0200

    updated module pkg waybar

--- roles/waybar/files/waybar/modules/pkg.sh
+++ roles/waybar/files/waybar/modules/pkg.sh
@@ -2,32 +2,34 @@
 
 pacman_check() {
     pac=$(checkupdates 2>/dev/null)
-    pac_count=$(wc -l <<< "$pac")
+    if test -n "$pac"; then pac_count=$(wc -l <<< "$pac"); fi
     aur=$(trizen -Qua 2>/dev/null)
-    aur_count=$(wc -l <<< "$aur")
+    if test -n "$aur"; then aur_count=$(wc -l <<< "$aur"); fi
     
-    if [[ $pac_count -eq "0" ]] && [[ $aur_count -eq "0" ]] || [[ ! $pac_count =~ [0-9]+ ]] || [[ ! $aur_count =~ [0-9]+ ]]
-    then
-        exit 0
-    fi
+    if test -z "$pac_count" && test -z "$aur_count"; then exit; fi
 
     text="$pac_count <span foreground='#929292'></span> $aur_count"
-    tooltip=$(awk -v ORS=' ' '$0=$1' <<< "$pac$aur")
+    tooltip_pac="pacman:\n\t$(awk -v ORS=' ' '$0=$1' <<< "$pac")"
+    tooltip_aur="aur:\n\t$(awk -v ORS=' ' '$0=$1' <<< "$aur")"
+    tooltip+=$(if test -n "$pac"; then echo "$tooltip_pac"; fi)
+    tooltip+=$(if test -n "$pac" && test -n "$aur"; then echo "\n"; fi)
+    tooltip+=$(if test -n "$aur"; then echo "$tooltip_aur"; fi)
 }
 
 xbps_check(){
-    pkg=$(xbps-install -SMnu 2>/dev/null)
-    pkg_count=$(xbps-install -SMnu 2>/dev/null | wc -l)
+    xbps=$(xbps-install -SMnu 2>/dev/null)
+    if test -n "$xbps"; then xbps_count=$(xbps-install -SMnu 2>/dev/null | wc -l); fi
     flatpak=$(flatpak remote-ls --updates --columns=name)
-    flatpak_count=$(flatpak remote-ls --updates --columns=name | wc -l)
+    if test -n "$flatpak"; then flatpak_count=$(flatpak remote-ls --updates --columns=name | wc -l); fi
 
-    if [[ $pkg_count -eq "0" ]] && [[ $flatpak_count -eq "0" ]] 
-    then
-        exit 0
-    fi
+    if test -z "$xbps_count" && test -z "$flatpak_count"; then exit; fi
 
-    text="$pkg_count <span foreground='#929292'></span> $flatpak_count"
-    tooltip=$(awk -v ORS=' ' '$0=$1' <<< "$pkg $flatpak")
+    text="$xbps_count <span foreground='#929292'></span> $flatpak_count"
+    tooltip_xbps="xbps:\n\t$(awk -v ORS=' ' '$0=$1' <<< "$xbps")"
+    tooltip_flat="flatpak:\n\t$(awk -v ORS=' ' '$0=$1' <<< "$flatpak")"
+    tooltip+=$(if test -n "$xbps"; then echo "$tooltip_xbps"; fi)
+    tooltip+=$(if test -n "$xbps" && test -n "$flatpak"; then echo "\n"; fi)
+    tooltip+=$(if test -n "$flatpak"; then echo "$tooltip_flat"; fi)
 }
 
 if grep -i "arch" /etc/os-release &>/dev/null
