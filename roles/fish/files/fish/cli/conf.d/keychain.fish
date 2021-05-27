dda349344e7afd9bc49f7f1bdea55e56ef476898
eoli3n <jonathan.kirszling@runbox.com>
2021-05-27 16:33:22 +0200

    added gpg-agent to keychain

--- roles/fish/files/fish/cli/conf.d/keychain.fish
+++ roles/fish/files/fish/cli/conf.d/keychain.fish
@@ -1,4 +1,5 @@
 # https://github.com/fish-shell/fish-shell/issues/4583
 if status --is-interactive
-    keychain --eval --quiet -Q id_rsa | source
+    keychain --eval --agents ssh --quiet -Q id_rsa | source
+    keychain --eval --agents gpg --quiet --gpg2 -Q | source
 end
