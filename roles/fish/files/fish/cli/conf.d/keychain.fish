ddac4e3ec2fad470bee51d276c732e5d7983231a
eoli3n <jonathan.kirszling@runbox.com>
2021-05-27 22:47:57 +0200

    added TTY_GPG env var to keychain.fish

--- roles/fish/files/fish/cli/conf.d/keychain.fish
+++ roles/fish/files/fish/cli/conf.d/keychain.fish
@@ -1,3 +1,6 @@
+# https://stackoverflow.com/questions/39494631/gpg-failed-to-sign-the-data-fatal-failed-to-write-commit-object-git-2-10-0
+set -x GPG_TTY (tty)
+
 # https://github.com/fish-shell/fish-shell/issues/4583
 if status --is-interactive
     keychain --eval --agents ssh --quiet -Q id_rsa | source
