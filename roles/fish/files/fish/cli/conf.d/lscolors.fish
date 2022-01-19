ce542cbf423e0b61bfb718273028cb5fed8bd615
eoli3n <jonathan.kirszling@runbox.com>
2022-01-19 14:53:34 +0100

    fix dir colors

--- roles/fish/files/fish/cli/conf.d/lscolors.fish
+++ roles/fish/files/fish/cli/conf.d/lscolors.fish
@@ -1,1 +1,5 @@
-eval (dircolors -c ~/.config/dircolors)
+if not set -q LS_COLORS
+    if type -f dircolors >/dev/null
+        eval (dircolors -c ~/.dircolors | sed 's/>&\/dev\/null$//')
+    end
+end
